92f1c31df224a1237147802e7731e85478795c92
dell <elocklymuhamed@gmail.com>
2024-01-21 22:46:11 +0200

    access cloud fire store data

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,70 +1,91 @@
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/widgets/custo_chat_pubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection('messages');
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              kLogo,
-              height: 70,
-            ),
-            Text(
-              'Chat',
-              style: TextStyle(fontSize: 24),
-            )
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return CustomChatPubble();
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Send Message',
-                suffixIcon: Icon(
-                  Icons.send,
-                  color: kPrimaryColor,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    kLogo,
+                    height: 70,
                   ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor,
-                  ),
-                ),
+                  Text(
+                    'Chat',
+                    style: TextStyle(fontSize: 24),
+                  )
+                ],
               ),
+              centerTitle: true,
             ),
-          ),
-        ],
-      ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return CustomChatPubble();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (value) {
+                      messages.add(
+                        {'message': value},
+                      );
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Send Message',
+                      suffixIcon: Icon(
+                        Icons.send,
+                        color: kPrimaryColor,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Text('Loading....');
+        }
+      },
     );
   }
 }
